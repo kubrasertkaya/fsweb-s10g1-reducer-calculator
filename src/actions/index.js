f581511a6aa94d6fab338180ae0b702567210014
kubrasertkaya <118736856+kubrasertkaya@users.noreply.github.com>
2023-06-02 18:55:18 +0300

    Calculator yapıldı.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,20 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export  const CE="CE Clear Display";
+
+export const MEMORY_PLUS="MEMORY_PLUS hafızaya at";
+export const MEMORY_RECORD="MEMORY_RECAL hafızadaki değeri ekrandaki ile toplar";
+export const MEMORY_CLEAR="MEMORY_CLEAR hafızayı temizle";
 
 export const addOne = () => {
   return ({ type: ADD_ONE });
 }
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
+  return ({ type: APPLY_NUMBER, payload: Number(number) });
+}
+
+export const changeOperation = (operation) => {
+  return ({ type: CHANGE_OPERATION, payload: operation });
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CE, MEMORY_PLUS, MEMORY_RECORD, MEMORY_CLEAR } from './../actions';
 
 export const initialState = {
-  total: 100,
-  operation: "*",
-  memory: 100
+  total: 0,
+  operation: "-",
+  memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -38,6 +38,31 @@ const reducer = (state, action) => {
         ...state,
         operation: action.payload
       });
+
+    case CE:
+      return{
+        ...state,
+        total:initialState.total,
+      }
+
+    case MEMORY_PLUS:
+      return {
+        ...state,
+        memory: state.total
+      };
+
+    case MEMORY_RECORD:
+      return {
+        ...state,
+        total: calculateResult(state.total, state.memory, state.operation),
+      };
+
+    case MEMORY_CLEAR:
+      return {
+        ...state,
+        memory: initialState.memory
+      };
+
 
     default:
       return state;
